3cbe6bf20df19978cf0d320c92c4ed862a9ab2d2
Tanzeel Hasan <88790783+ErTanzeel@users.noreply.github.com>
2022-10-18 20:17:50 +0530

    Add files via upload

---
+++ scripts/deparmnetAdd.js
@@ -0,0 +1,28 @@
+// Importing Department Model to add new Department
+import { DepartmentModel } from "../model/BaseModel.js";
+// Importing Department Implementation to Perform department Related Operation
+import DepartmentImplementation from "../Implementation/DepartmentImpementation.js";
+
+
+// input tag to get the name of the department
+let name = document.getElementById("deptName");
+// Department Implementation  Object to Perform All the Department Related  Operation
+let implementation = new DepartmentImplementation();
+
+
+//
+document.getElementById("submit").addEventListener("click", (event) => {
+  //    to prevent the Default behavior of Form here , we using prevent Default
+  event.preventDefault();
+  // creating Model of Department
+  let department = new DepartmentModel(name.value);
+  implementation.Add(department);
+  console.log(implementation);
+
+  alert("added successfully");
+
+  name.value = "";
+  window.location.replace('../pages/departmnet.html')
+
+});
+
