8d47f4e9dd3aa985bb8ad71a17b004984036d073
Tanzeel Hasan <88790783+ErTanzeel@users.noreply.github.com>
2022-10-18 20:21:32 +0530

    Add files via upload

---
+++ scripts/pagination.js
@@ -0,0 +1,22 @@
+//   Here Working OF Pagination is Defined 
+// It is Takes An Array Pages and Rows
+export default function pagination(dataSet, page, rows) {
+
+    let startIndex = (page - 1) * rows;
+    console.log()
+    let endIndex = startIndex + rows;
+
+  
+    let trimmedData = dataSet.slice(startIndex, endIndex);
+  
+    let pages = Math.ceil(dataSet.length / rows);
+  
+    // In Return Getting Trimmed Data and pages
+    return {
+      dataSet: trimmedData,
+      pages: pages,
+    };
+    
+  }
+
+
